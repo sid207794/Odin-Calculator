325d7054bc1eb5f6b79e4846fa1f31d1afc5a320
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-04 08:26:14 +0000

    Add for loop to iterate over each button id and add two event handlers 'mouseenter' and 'mouseleave' to each button

--- script.js
+++ script.js
@@ -31,12 +31,15 @@ const operate = function (a, b, c) {
 };
 
 const container = document.querySelector("#container");
+
 const array = ["AC", "%", "⌫", "÷", "7", "8", "9", "×", "4", "5", "6", "–", "1", "2", "3", "+", "00", "0", ".", "="];
+
 for (i=0; i<20; i++) {
     const element = document.createElement("div");
     element.setAttribute("id", `key${i+1}`);
     element.style.height = `${400/5}px`;
     element.style.width = `${350/4}px`;
+
     container.appendChild(element);
 
     const key = document.querySelector(`#key${i+1}`);
@@ -44,5 +47,50 @@ for (i=0; i<20; i++) {
     const button = document.createElement("button");
     button.setAttribute("id", `press${i+1}`);
     button.textContent = array[i];
+
     key.appendChild(button);
+}
+
+for (i=1; i<=20; i++) {
+    if (i === 1 || i === 2 || i === 3) {
+        const press = document.querySelector(`#press${i}`);
+
+        press.addEventListener("mouseenter", () => {
+            press.style.backgroundColor = "rgba(255, 255, 255, 0.17)";
+        });
+
+        press.addEventListener("mouseleave", () => {
+            press.style.backgroundColor = "rgb(25, 26, 25)";
+        });
+    } else if (i%4 === 0 && i<20 && i>0) {
+        const press = document.querySelector(`#press${i}`);
+
+        press.addEventListener("mouseenter", () => {
+            press.style.backgroundColor = "rgba(255, 255, 255, 0.17)";
+        });
+
+        press.addEventListener("mouseleave", () => {
+            press.style.backgroundColor = "rgb(25, 26, 25)";
+        });
+    } else if (i === 20) {
+        const press = document.querySelector(`#press${i}`);
+
+        press.addEventListener("mouseenter", () => {
+            press.style.backgroundColor = "rgba(255, 70, 0, 0.85)";
+        });
+
+        press.addEventListener("mouseleave", () => {
+            press.style.backgroundColor = "rgb(255, 70, 0)";
+        });
+    } else {
+        const press = document.querySelector(`#press${i}`);
+
+        press.addEventListener("mouseenter", () => {
+            press.style.backgroundColor = "rgba(255, 255, 255, 0.25)";
+        });
+
+        press.addEventListener("mouseleave", () => {
+            press.style.backgroundColor = "rgb(39, 40, 39)";
+        });
+    }
 }
