3fae633a87073dd4a986ab58ef5074a6edb8b6a6
Siddharth Singh <193688659+sid207794@users.noreply.github.com>
2025-03-13 09:29:19 +0000

    Remove 'for' loop created for event handlers to change background colors of .headButton elements and 'for' loop created for event handlers to change background color, font-size and button height of all grand-child elements of #container

--- script.js
+++ script.js
@@ -39,19 +39,6 @@ const operate = function (a, b, c) {
     }
 };
 
-// DOM style for element with id = "head"
-const headButton = document.querySelectorAll(".headButton");
-
-for (const headButt of headButton) {
-    headButt.addEventListener("mouseenter", () => {
-        headButt.style.backgroundColor = "rgba(255, 255, 255, 0.1";
-    });
-
-    headButt.addEventListener("mouseleave", () => {
-        headButt.style.backgroundColor = "transparent";
-    });
-}
-
 // DOM style for creating child elements of id = "container"
 const container = document.querySelector("#container");
 
@@ -72,67 +59,6 @@ for (i=0; i<20; i++) {
     button.textContent = array[i];
 
     key.appendChild(button);
-}
-
-// DOM style for animating child elements of id = "container"
-for (i=1; i<=20; i++) {
-    if (i === 1 || i === 2 || i === 3) {
-        const press = document.querySelector(`#press${i}`);
-
-        press.addEventListener("mouseenter", () => {
-            press.style.backgroundColor = "rgba(255, 255, 255, 0.17)";
-            press.style.height = "60px";
-            press.style.fontSize = `${(60/68)*20}px`;
-        });
-
-        press.addEventListener("mouseleave", () => {
-            press.style.backgroundColor = "rgb(25, 26, 25)";
-            press.style.height = "68px";
-            press.style.fontSize = "20px";
-        });
-    } else if (i%4 === 0 && i<20 && i>0) {
-        const press = document.querySelector(`#press${i}`);
-
-        press.addEventListener("mouseenter", () => {
-            press.style.backgroundColor = "rgba(255, 255, 255, 0.17)";
-            press.style.height = "60px";
-            press.style.fontSize = `${(60/68)*38}px`;
-        });
-
-        press.addEventListener("mouseleave", () => {
-            press.style.backgroundColor = "rgb(25, 26, 25)";
-            press.style.height = "68px";
-            press.style.fontSize = "38px";
-        });
-    } else if (i === 20) {
-        const press = document.querySelector(`#press${i}`);
-
-        press.addEventListener("mouseenter", () => {
-            press.style.backgroundColor = "rgba(255, 70, 0, 0.85)";
-            press.style.height = "60px";
-            press.style.fontSize = `${(60/68)*40}px`;
-        });
-
-        press.addEventListener("mouseleave", () => {
-            press.style.backgroundColor = "rgb(255, 70, 0)";
-            press.style.height = "68px";
-            press.style.fontSize = "40px";
-        });
-    } else {
-        const press = document.querySelector(`#press${i}`);
-
-        press.addEventListener("mouseenter", () => {
-            press.style.backgroundColor = "rgba(255, 255, 255, 0.25)";
-            press.style.height = "60px";
-            press.style.fontSize = `${(60/68)*30}px`;
-        });
-
-        press.addEventListener("mouseleave", () => {
-            press.style.backgroundColor = "rgb(39, 40, 39)";
-            press.style.height = "68px";
-            press.style.fontSize = "30px";
-        });
-    }
 }
 
 const display = document.querySelector("#display");
